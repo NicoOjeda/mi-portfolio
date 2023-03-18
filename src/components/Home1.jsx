5ef59dfb2bcae09fb470732c32fb9f1ea66c4c5d
Nico ojeda <nico_ojeda86@hotmail.com>
2023-03-17 23:14:31 -0300

    se pone animacion a titulo

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../styles/Home1.css'
-import Header from './Header'
+import Navbar from './Navbar'
 
 export default function Home1() {
     return (
         <div className='Home1-container'>
-            <Header/>
+            <Navbar/>
             <div className='Home1-box'>
-                <h2>Nicolas Ojeda</h2>
-                <h4>Desarrollador FullStack</h4>
+                <h2 className='focus-in-contract '>Nicolas Ojeda</h2>
+                <h4 className='focus-in-contract2'>Desarrollador FullStack</h4>
             </div>   
         </div>
     )
