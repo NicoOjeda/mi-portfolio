834d277dee60703dc1bbeb39a2df199f8285d0ad
Nico ojeda <nico_ojeda86@hotmail.com>
2023-03-23 23:36:55 -0300

    se coloca id home para linkear

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar'
 
 export default function Home1() {
     return (
-        <div className='Home1-container'>
+        <div className='Home1-container' id='home'>
             <Navbar/>
             <div className='Home1-box'>
                 <p className='focus-in-contract2'>Hola!</p>
