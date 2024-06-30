9e950dad4d368c3d8155bd497d6300128a107240
NicoOjeda <nico_ojeda86@hotmail.com>
2024-06-30 18:44:21 -0300

    se descomentan comentarios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-// import React from 'react'
-// import About from '../components/About'
-// import Home1 from '../components/Home1'
-// import Proyects from '../components/Proyects'
+import React from 'react'
+import About from '../components/About'
+import Home1 from '../components/Home1'
+import Proyects from '../components/Proyects'
 
 export default function Home() {
   return (
     <div>
-        {/* <Home1/>
+        <Home1/>
         <About/>
-        <Proyects/> */}
+        <Proyects/>
     </div>
 )
 }
