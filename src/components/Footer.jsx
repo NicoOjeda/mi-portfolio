ea29df4d7e0691a64d63a060aa4149feaeb7c4a5
NicoOjeda <nico_ojeda86@hotmail.com>
2024-07-20 22:36:45 -0300

    se define componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='Footer-container'> Nicolas Ojeda © 2024 </div>
+  )
+}
+
+export default Footer
